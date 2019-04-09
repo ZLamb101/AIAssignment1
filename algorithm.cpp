3ae905ef0ee8d1e523447ac6d0d9a0c48f116433
Bupton64 <bupton@hotmail.co.nz>
2019-04-09 17:40:03 +1200

    Implemented heap for UC

--- algorithm.cpp
+++ algorithm.cpp
@@ -234,7 +234,7 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 	startTime = clock();
 	Puzzle * currentState = new Puzzle(initialState, goalState);
     Puzzle * nextState;
-    vector<Puzzle*>  *Q = new vector<Puzzle*>();
+    Heap  *Q = new Heap();
     HashTable *ExpandedList = new HashTable();
     numOfStateExpansions = 0;
     maxQLength = 0;
@@ -248,18 +248,8 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				break;
 			}
 			// Find next best cost
-			Puzzle* temp = Q->at(0);
-			int min = temp->getGCost();
-			int min_ix = 0;
-			for(int i = 0; i < Q->size(); i++){
-				temp = Q->at(i);
-				if(temp->getGCost() < min){
-					min = temp->getGCost();
-					min_ix = i;
-				}
-			}
-			currentState = Q->at(min_ix);
-			Q->erase(Q->begin() + min_ix);
+			currentState = Q->getRootData();
+			Q->deleteAtIndex(0);
 			continue;
 		} else {
 			numOfStateExpansions++;
@@ -267,20 +257,9 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				nextState = currentState->moveUp();
 				if(!currentState->checkExpansionPath(nextState->toString())){
 					if(ExpandedList->checkHashNoAdd(nextState->toString())){
-						bool notInQ = true;
-						for(int i = 0; i < Q->size(); i++){
-							Puzzle* temp = Q->at(i);
-							if(temp->toString() == nextState->toString()){
-								notInQ = false;
-								if(temp->getGCost() > nextState->getGCost()){
-									Q->erase(Q->begin() + i);
-									Q->emplace(Q->begin(), nextState);
-									break;
-								}
-							}
-						}
-						if(notInQ){
-							Q->emplace(Q->begin(), nextState);
+						nextState->updateFCost();
+						if(!Q->checkHeap(nextState->toString(), nextState->getFCost())){
+							Q->InsertHeap(nextState);
 						}
 					}
 				}
@@ -289,20 +268,9 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				nextState = currentState->moveRight();
 				if(!currentState->checkExpansionPath(nextState->toString())){
 					if(ExpandedList->checkHashNoAdd(nextState->toString())){
-						bool notInQ = true;
-						for(int i = 0; i < Q->size(); i++){
-							Puzzle* temp = Q->at(i);
-							if(temp->toString() == nextState->toString()){
-								notInQ = false;
-								if(temp->getGCost() > nextState->getGCost()){
-									Q->erase(Q->begin() + i);
-									Q->emplace(Q->begin(), nextState);
-									break;
-								}
-							}
-						}
-						if(notInQ){
-							Q->emplace(Q->begin(), nextState);
+						nextState->updateFCost();
+						if(!Q->checkHeap(nextState->toString(), nextState->getFCost())){
+							Q->InsertHeap(nextState);
 						}
 					}
 				}
@@ -311,20 +279,9 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				nextState = currentState->moveDown();
 				if(!currentState->checkExpansionPath(nextState->toString())){
 					if(ExpandedList->checkHashNoAdd(nextState->toString())){
-						bool notInQ = true;
-						for(int i = 0; i < Q->size(); i++){
-							Puzzle* temp = Q->at(i);
-							if(temp->toString() == nextState->toString()){
-								notInQ = false;
-								if(temp->getGCost() > nextState->getGCost()){
-									Q->erase(Q->begin() + i);
-									Q->emplace(Q->begin(), nextState);
-									break;
-								}
-							}
-						}
-						if(notInQ){
-							Q->emplace(Q->begin(), nextState);
+						nextState->updateFCost();
+						if(!Q->checkHeap(nextState->toString(), nextState->getFCost())){
+							Q->InsertHeap(nextState);
 						}
 					}
 				}
@@ -333,20 +290,9 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				nextState = currentState->moveLeft();
 				if(!currentState->checkExpansionPath(nextState->toString())){
 					if(ExpandedList->checkHashNoAdd(nextState->toString())){
-						bool notInQ = true;
-						for(int i = 0; i < Q->size(); i++){
-							Puzzle* temp = Q->at(i);
-							if(temp->toString() == nextState->toString()){
-								notInQ = false;
-								if(temp->getGCost() > nextState->getGCost()){
-									Q->erase(Q->begin() + i);
-									Q->emplace(Q->begin(), nextState);
-									break;
-								}
-							}
-						}
-						if(notInQ){
-							Q->emplace(Q->begin(), nextState);
+						nextState->updateFCost();
+						if(!Q->checkHeap(nextState->toString(), nextState->getFCost())){
+							Q->InsertHeap(nextState);
 						}
 					}
 				}
@@ -360,18 +306,8 @@ string uniformCost_ExpandedList(string const initialState, string const goalStat
 				break;
 			}
 			// Find next best cost
-			Puzzle* temp = Q->at(0);
-			int min = temp->getGCost();
-			int min_ix = 0;
-			for(int i = 0; i < Q->size(); i++){
-				temp = Q->at(i);
-				if(temp->getGCost() < min){
-					min = temp->getGCost();
-					min_ix = i;
-				}
-			}
-			currentState = Q->at(min_ix);
-			Q->erase(Q->begin() + min_ix);
+			currentState = Q->getRootData();
+			Q->deleteAtIndex(0);
 		}
 	}
 
@@ -405,7 +341,6 @@ string aStar_ExpandedList(string const initialState, string const goalState, int
     numOfAttemptedNodeReExpansions=0;
 	actualRunningTime=0.0;	
 	startTime = clock();
-	Heap* test = new Heap();
 	Puzzle * currentState = new Puzzle(initialState, goalState);
     Puzzle * nextState;
     Heap  *Q = new Heap();
